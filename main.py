a45584a19d0e9f042425542e25b191eeb515b5c5
Mr-argha-das <hdark6336@gmail.com>
2025-01-09 22:14:29 +0530

    changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from artist.routes import artists_routes
 from songs.routes import songsrouts
 connect('MAINSONGSDATABASE', host="mongodb+srv://avbigbuddy:nZ4ATPTwJjzYnm20@cluster0.wplpkxz.mongodb.net/MAINSONGSDATABASE")
 app = FastAPI()
-app.include_router(addartists.router, tags=["add Artists"])
+# app.include_router(addartists.router, tags=["add Artists"])
 app.include_router(artists_routes.router, tags=["Artist"])
 app.include_router(songsrouts.router, tags=["songs routes"])
 import uvicorn
